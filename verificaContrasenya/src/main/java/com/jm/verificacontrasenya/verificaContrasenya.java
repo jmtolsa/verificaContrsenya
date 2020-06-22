3b0925510e17d2e0609963b7ceb13eebcac0ba97
Josep Mª <jtolsa@xtec.cat>
2020-06-22 12:26:42 +0200

    versió 2

--- verificaContrasenya/src/main/java/com/jm/verificacontrasenya/verificaContrasenya.java
+++ verificaContrasenya/src/main/java/com/jm/verificacontrasenya/verificaContrasenya.java
@@ -20,28 +20,42 @@ public class verificaContrasenya {
         String caractersAdmesos = "abcdefghijklmnopqrstuvwxyzñç";
         String numerosAdmesos = "1234567890";
         
-        String contrasenya = "aaaaa22aa";
+        String contrasenya = "aaAaa22a$aaa";
         
-        if (contrasenya.length()<8){
-            System.out.println("La cotrasenya es massa curta");
+        if (contrasenya.length()<10){
+            System.out.println("La cotrasenya es massa curta, mínim 10");
         }
         else{
-            int algunaLletra = 0;
+            int algunaLletraMinuscula = 0;
+            int algunaLletraMajuscula = 0;
             int algunNumero = 0;
+            int algunSimbol = 0;
             for (int posicio=0;posicio<contrasenya.length();posicio++){
                 if (caractersAdmesos.indexOf(contrasenya.charAt(posicio))>=0){
-                    algunaLletra = 1;
+                    algunaLletraMinuscula += 1;
+                }else if (caractersAdmesos.toUpperCase().indexOf(contrasenya.charAt(posicio))>=0){
+                    algunaLletraMajuscula += 1;
+                }else if (numerosAdmesos.indexOf(contrasenya.charAt(posicio))>=0){
+                    algunNumero += 1;
                 }
-                if (numerosAdmesos.indexOf(contrasenya.charAt(posicio))>=0){
-                    algunNumero = 1;
+                else if (posicio > 0 || posicio < (contrasenya.length()))
+                {
+                    algunSimbol +=1;
                 }
             }
-            if (algunaLletra == 0){
-                System.out.println("La cotrasenya ha de contenir alguna lletra");
+            if (algunaLletraMinuscula == 0){
+                System.out.println("La cotrasenya ha de contenir alguna lletra minuscula");
             }
-            else if (algunNumero == 0){
-                System.out.println("La cotrasenya ha de contenir algun numero");
-            }else  
+            if (algunaLletraMajuscula == 0){
+                System.out.println("La cotrasenya ha de contenir alguna lletra majuscula");
+            }
+            else if (algunNumero < 2){
+                System.out.println("La cotrasenya ha de contenir almenys 2 numeros");
+            }else if (algunSimbol == 0) 
+            {
+                System.out.println("La cotrasenya ha de contenir algun simbol que no estigui al principi ni al final");
+            }
+            else
             {
                 System.out.println("Contrasenya correcta");
             }
